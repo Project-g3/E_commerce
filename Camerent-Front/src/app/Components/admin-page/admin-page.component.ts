5d2a63cbf984f74fe6401631eaa9319a92ba0c53
SjdSaj <sjdsaj63@gmail.com>
2022-07-22 23:21:01 +0530

    admin sidebar v0.1

--- Camerent-Front/src/app/Components/admin-page/admin-page.component.ts
+++ Camerent-Front/src/app/Components/admin-page/admin-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { navData } from './nav-data';
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent implements OnInit {
-
+  collapsed = false;
+  navData = navData;
   constructor() { }
 
   ngOnInit(): void {
