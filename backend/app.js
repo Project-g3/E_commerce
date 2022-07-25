2d97abae30cc9fcba22d4152efc88c70aea73795
SjdSaj <sjdsaj63@gmail.com>
2022-07-25 19:33:43 +0530

    Backend folder created

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = new express();
+
+
+
+
+// for connecting with frontend
+app.use(cors);
+
+
+
+app.get('/',(req,res)=>{
+  res.send('working fine');
+});
+
+app.listen(3000);
+console.log('the server is running on port:3000');
