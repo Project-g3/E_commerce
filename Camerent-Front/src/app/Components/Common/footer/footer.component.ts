7e110e3a9d2373a1a460c0ebe90f27adc6f60c79
SjdSaj <sjdsaj63@gmail.com>
2022-07-26 21:02:27 +0530

    footer completed design

--- Camerent-Front/src/app/Components/Common/footer/footer.component.ts
+++ Camerent-Front/src/app/Components/Common/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { footerData } from './footer.data';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  footerNav = footerData;
 
   constructor() { }
 
---
+++ Camerent-Front/src/app/Components/Common/footer/footer.data.ts
@@ -0,0 +1,24 @@
+export const footerData = [
+        {
+          name:'Home',
+          link:''
+        }, 
+        {
+          name:'Camera',
+          link:''
+        },
+        {
+          name:'Accessories',
+          link:''
+        },
+        {
+          name:'About',
+          link:''
+        },
+        {
+          name:'ContactUs',
+          link:''
+        }
+      ]
+
+
