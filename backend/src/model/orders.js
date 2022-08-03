19b6f7fb52e5e9511a9e1628ad5256485fa479a4
sajad <sjdsaj63@gmail.com>
2022-08-03 16:20:38 +0530

    created schema for orders

---
+++ backend/src/model/orders.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/Camerent');
+
+// creating a schema for products
+
+var schema = mongoose.Schema;
+
+const orders = new schema({
+    orderId : String,
+    customerId : String,
+    customerName: String,
+    customerNumber : Number,
+    customerEmailId : String,
+    price : Number,
+    productId : String,
+    modeOfPurchase : String, // rent or buy
+    fromDate : String,       // rent start date
+    toDate : String,         // rent end date
+
+
+});
+
+var orderData = mongoose.model('order',orders);
+module.exports = orderData;
+
+
