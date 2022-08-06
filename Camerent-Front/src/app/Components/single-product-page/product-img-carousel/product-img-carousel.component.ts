82372728640d14f5867dd3ae5abf14f98d578e9f
sajad <sjdsaj63@gmail.com>
2022-08-06 15:34:13 +0530

    Single product image hover set

--- Camerent-Front/src/app/Components/single-product-page/product-img-carousel/product-img-carousel.component.ts
+++ Camerent-Front/src/app/Components/single-product-page/product-img-carousel/product-img-carousel.component.ts
@@ -6,7 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-img-carousel.component.css']
 })
 export class ProductImgCarouselComponent implements OnInit {
-  array = new Array(4);
+  array = [
+  {
+    imgurl:'cam1.jpeg',
+    index:1
+  },
+  {
+    imgurl:'cam2.jpeg',
+    index:2
+  },
+  {
+    imgurl:'cam3.jpeg',
+    index:3
+  },
+  {
+    imgurl:'cam4.jpeg',
+    index:4
+  },
+  {
+    imgurl:'cam5.jpeg',
+    index:5
+  },
+  {
+    imgurl:'cam6.jpeg',
+    index:6
+  }
+];
+
+  mouseenter(abc:any,mainImg:any){
+    console.log('mouse enter',abc);
+    mainImg.src='assets/productImg/'+abc;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
