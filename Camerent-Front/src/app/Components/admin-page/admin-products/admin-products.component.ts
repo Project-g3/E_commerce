157ed3a9d74828d534b80399dfa36aa2c14929f8
sajad <sjdsaj63@gmail.com>
2022-08-06 13:47:53 +0530

    filtering admin products 80%

--- Camerent-Front/src/app/Components/admin-page/admin-products/admin-products.component.ts
+++ Camerent-Front/src/app/Components/admin-page/admin-products/admin-products.component.ts
@@ -11,9 +11,15 @@ import { Products } from './admin-products.model';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
-  
-  productsList : Products[] | undefined;
+
+  // intializing variable for two way data binding with ngModel
+  searchbyid = '';
+  searchbyname = '';
+  searchbycat = '';
+
+  productsList : any;
   products :any;
+
 
   // category 
   category=[
@@ -23,26 +29,63 @@ export class AdminProductsComponent implements OnInit {
   ]
 
   // function for searching by category
-  searchByCategory(value:any){
-    this.products.forEach((a:any)=>{
+  // searchByCategory(value:any){
+  //   this.products.forEach((a:any)=>{
       
-      if(a.category===value){
-        this.products.push(a);
-        console.log(a);
-      }
+  //     if(a.category===value){
+  //       this.products.push(a);
+  //       console.log(a);
+  //     }
 
-    })
-    this.productsList = this.products
+  //   })
+  //   this.productsList = this.products
+  // }
+
+  searchFilter(name:any){
+    let text = this.searchbyname.trim().toLocaleLowerCase();
+    let temp  = new Array;
+    temp = this.products;
+    
+    // fileter by name
+    if(name.id==='searchName'){
+      console.log('filter by name')
+      if (text === '') {
+        this.productsList = this.products;
+      } else {
+        this.productsList = [];
+
+        temp.map((a) => {
+          if (a.name.toLocaleLowerCase().includes(text)) {
+            this.productsList.push(a);
+          }
+        })
+
+      }
+      // filter by category 
+    }else if(name.id==='searchCategory'){
+      let text = this.searchbycat.trim().toLocaleLowerCase();
+      console.log('category filter');
+      if (text === '') {
+        this.productsList = this.products;
+      } else {
+        this.productsList = [];
+
+        temp.map((a) => {
+          if (a.category.toLocaleLowerCase().includes(text)) {
+            this.productsList.push(a);
+          }
+        })
+
+      }
+    }
+    
   }
   
 
 
 
 
-  // intializing variable for two way data binding with ngModel
-  searchbyid = '';
-  searchbyname = '';
-  searchbycat = '';
+
   
   
     
