f426369562a5c7dc95a37769e6c1dc690f1c5cf1
sajad <sjdsaj63@gmail.com>
2022-08-15 20:52:24 +0530

    images taken from database and placed in single products page

--- Camerent-Front/src/app/Components/single-product-page/product-img-carousel/product-img-carousel.component.ts
+++ Camerent-Front/src/app/Components/single-product-page/product-img-carousel/product-img-carousel.component.ts
@@ -9,29 +9,6 @@ export class ProductImgCarouselComponent implements OnInit {
 
   @Input() product : any;
   
-  array = [
-  {
-    imgurl:'cam1.jpeg',
-    index:1
-  },
-  {
-    imgurl:'cam2.jpeg',
-    index:2
-  },
-  {
-    imgurl:'cam3.jpeg',
-    index:3
-  },
-  {
-    imgurl:'cam4.jpeg',
-    index:4
-  },
-  {
-    imgurl:'cam5.jpeg',
-    index:5
-  }
-  
-];
 
   // setting img in hover 
   mouseenter(i:any,mainImg:any){
